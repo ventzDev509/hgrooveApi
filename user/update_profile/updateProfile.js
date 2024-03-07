8ac6feee9f00930d207ded5e016abf84b3bcd3dc
Marceille Eventz <156130198+ventzDev509@users.noreply.github.com>
2024-03-07 15:04:09 -0800

    Add files via upload

--- user/update_profile/updateProfile.js
+++ user/update_profile/updateProfile.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 module.exports = (app) => {
     app.post("/api/hgroove/v1/update-profile", isLogin, (req, res) => {
         const userLogin = req.userEmail;
-        const path = process.env.LINK_PHP + "profile/" + req.body.profile
+        const path = process.env.LINK_PHP + "profileUser/" + req.body.profile
         USER.update({ profile: path }, { where: { email: userLogin } })
             .then(response => {
                 res.json({ message: "Your profile as been Updating" })
